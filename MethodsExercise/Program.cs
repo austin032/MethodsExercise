8576c1407298eb285f2772bea76961a00f54f13b
Austin Okyere <okyereaustin@gmail.com>
2025-09-16 16:12:15 -0600

    Completed C# story program and math operations program

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -15,9 +15,47 @@
             
             Console.WriteLine($"Over the weekend {name} was out playing {sport}, after nine holes he stopped and grabbed a {food}. When he was finished playing he winded down by watching the {tv}.");
         }
+
+        public static int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public static int Subtract(int a, int b)
+        {
+            return a - b;
+        }
+
+        public static int Multiply(int a, int b)
+        {
+            return a * b;
+        }
+
+        public static int Divide(int a, int b)
+        {
+            return a / b;
+        }
+
+        public static int Sum(params int[] numbers)
+        {
+            int sum = 0;
+
+            foreach (int n in numbers)
+            {
+                sum += n;
+            }
+            
+            return sum;
+        }
+        
         static void Main(string[] args)
         {
-            MadLib();
+            //MadLib();
+            //Console.WriteLine(Add(4, 4));
+            //Console.WriteLine(Subtract(6, 4));
+            //Console.WriteLine(Multiply(5, 5));
+            //Console.WriteLine(Divide(10, 2));
+            Console.WriteLine(Sum(3, 3, 3, 3, 3, 3, 3, 3));
         }
     }
 }
--- MethodsExercise/obj/Debug/net9.0/MethodsExercise.AssemblyInfo.cs
+++ MethodsExercise/obj/Debug/net9.0/MethodsExercise.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MethodsExercise")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+879bc51ef403450a0a853307a5e2bad9e738048e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8a7e814dd396cd137b68fdd7fffe4ffeb6e078ee")]
 [assembly: System.Reflection.AssemblyProductAttribute("MethodsExercise")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MethodsExercise")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
